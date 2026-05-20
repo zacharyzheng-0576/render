03813c5de7186beb7b7b761a2d574b1e39b8c7a8
Zachary ZHENG <gaoxiaonanyaodushu@Zacharys-MacBook-Air.local>
2026-05-20 22:31:29 +0800

    configure firebase survey backend

--- docs/firebase-config.js
+++ docs/firebase-config.js
@@ -1,4 +1,12 @@
-window.CROSSPILOT_FIREBASE_CONFIG = null;
+window.CROSSPILOT_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBVXihMjbNwyR5Uyq37VlCWXAex1Yw-gf4",
+  authDomain: "crosspilot-survey.firebaseapp.com",
+  projectId: "crosspilot-survey",
+  storageBucket: "crosspilot-survey.firebasestorage.app",
+  messagingSenderId: "56303894366",
+  appId: "1:56303894366:web:6c4e3e811b2bd0778b59cf",
+  measurementId: "G-12BGFPD8ZM"
+};
 window.CROSSPILOT_ADMIN_EMAILS = [
-  'your-email@example.com'
+  'hambur.zach@gmail.com'
 ];
